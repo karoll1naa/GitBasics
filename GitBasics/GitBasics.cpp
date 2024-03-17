afeb960906b685cc8337803eb458a835390aae97
karoll1naa <karollinnajan@gmail.com>
2024-03-17 08:17:22 +0200

    Добавлена реалізація форматування(знаходження і заміна символів) з тексту у HTML формат

--- GitBasics/GitBasics.cpp
+++ GitBasics/GitBasics.cpp
@@ -3,14 +3,35 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <regex>
 
 using namespace std;
 
 string MarkdowntoHTML(const string& markdown) {
+	stringstream HTMLstream;
+	stringstream MarkdownStream(markdown);
+	string line;
+	while (getline(MarkdownStream, line)) {
+		regex boldRegex("\\*\\*(.*?)\\*\\*");
+		line = regex_replace(line, boldRegex, "<b>$1</b>");
 
-	///...
+		regex italicRegex("\\_(.*?)\\_");
+		line = regex_replace(line, italicRegex, "<i>$1</i>");
 
-		return markdown;
+		regex codeBlockRegex("```([^`]+)```");
+		line = regex_replace(line, codeBlockRegex, "<pre>$1</pre>");
+
+		regex codeRegex("`([^`]+)`");
+		line = regex_replace(line, codeRegex, "<tt>$1</tt>");
+
+		if (line.find("<") == string::npos && line.find(">") == string::npos) {
+			HTMLstream << "<p>" << line << "</p>";
+		}
+		else {
+			HTMLstream << line;
+		}
+	}
+	return HTMLstream.str();
 }
 
 string ReadFile(const string& filePath) {
