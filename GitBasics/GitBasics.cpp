c7cea04ec76ef6138efb5d2825b36f91393a5571
karoll1naa <karollinnajan@gmail.com>
2024-04-08 19:40:12 +0300

    Added some code to main

--- GitBasics/GitBasics.cpp
+++ GitBasics/GitBasics.cpp
@@ -70,37 +70,54 @@ string ReadFile(const string& filePath) {
 void WriteFile(const string& text, const string& filePath) {
 	ofstream file(filePath);
 	if (!file.is_open()) {
-		cerr << " Sorry, the file cannot be opened for reading " << filePath <<  endl;
+		cerr << " Sorry, the file cannot be opened for reading " << filePath << endl;
 		exit(EXIT_FAILURE);
 	}
 
 	file << text;
-	cout<< "The HTML format text was written to " << filePath <<endl;
+	cout << "The HTML format text was written to " << filePath << endl;
 }
 
 int main(int args, char* argv[])
 {
 	if (args < 2) {
-		cerr<<"Usage: "<<argv[0]<< "<input_file_path>[--out <output_file_path>]" << endl;
+		cerr << "Usage: " << argv[0] << "<input_file_path>  [--format=<value>] [--out <output_file_path>]" << endl;
 		return EXIT_FAILURE;
 	}
 
-	string inputFilePath = argv[1]; 
+	string inputFilePath = argv[1];
 	string outputFilePath;
-	bool WriteFileFlag = false; 
-	if (args == 4 && string(argv[2]) == "--out") {
-		outputFilePath = argv[3];
-		WriteFileFlag = true;
+	string format;
+	bool WriteFileFlag = false;
+	for (int i = 2; i < args; ++i) {
+		string arg = argv[i];
+		if (arg == "--out") {
+			if (i + 1 < args) {
+				outputFilePath = argv[i + 1];
+				WriteFileFlag = true;
+			}
+		}
+		else if (arg.find("--format=") == 0) {
+			format = arg.substr(9);
+		}
 	}
-
 	string Markdown = ReadFile(inputFilePath);
-	string HTML = MarkdowntoHTML(Markdown);
-
-	if (WriteFileFlag) {
-		WriteFile(HTML, outputFilePath);
+	string FormattedText;
+	if (format == "ansi" || format.empty()){
+		FormattedText = AnsiFormat(Markdown);
+	}
+	else if (format == "html" || !outputFilePath.empty()) {
+		FormattedText = MarkdowntoHTML(Markdown);
 	}
 	else {
-		cout <<HTML<<endl;
+		cerr << "Format error!" << endl;
+		return EXIT_FAILURE;
+	}
+	if (WriteFileFlag) {
+		WriteFile(FormattedText, outputFilePath);
+	}
+	else {
+		cout << FormattedText << endl;
 	}
 
 	return EXIT_SUCCESS;
